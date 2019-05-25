846ca462c86963fa760bbc29f187891fa0898f97
Harish Hardy <mj.harishardy@gmail.com>
2019-05-25 12:14:15 +0530

    not completed

--- pList/ViewController.swift
+++ pList/ViewController.swift
@@ -9,8 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+  
 
     override func viewDidLoad() {
+        
+        if let pathX = Bundle.main.path(forResource: "category", ofType: "plist")
+        {
+            print(pathX)
+            
+            if let arrayX = NSArray(contentsOfFile: pathX) as? [String: Any]
+            {
+                //print(dictionary)
+                //print(arrayX["cars"] ?? "not valid option")
+                //print(arrayX["smartPhones"] ?? "not valid option")
+                print(arrayX["programmingLanguages"] ?? "not valid option")
+            }
+            
+            if let dictionaryX = NSDictionary(contentsOfFile: pathX) as? [String: Any]
+            {
+                //print(dictionary)
+                print(dictionaryX["car"] ?? "not valid option")
+                print(dictionaryX["phone"] ?? "not valid option")
+                print(dictionaryX["language"] ?? "not valid option")
+                print(dictionaryX["cars"] ?? "not valid option")
+                print(dictionaryX["smartPhones"] ?? "not valid option")
+            }
+            
+        }
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
